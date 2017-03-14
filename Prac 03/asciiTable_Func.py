94ca788d89831c45ecf89f84aebe5e200bed4048
Micheal Brady-Mahoney <micheal.bradymahoney@my.jcu.edu.au>
2017-03-14 16:50:14 +1000

    ASCIItable asking for value

---
+++ Prac 03/asciiTable_Func.py
@@ -0,0 +1,58 @@
+__author__ = 'Micheal Brady-Mahoney'
+
+# character = str(input("Enter a character: "))
+# print("The ASCII code for {} is {}".format(character, ord(character)))
+
+# UPPER = 0
+# LOWER = 0
+
+
+def get_lower_upper():
+    lower = int(input("Enter a lower number between 33 and 127:"))
+    while lower < 33:
+        print("Please enter a valid number!")
+        lower = int(input("Enter a lower number between 33 and 127:"))
+    upper = int(input("Enter an upper number between {} and 127:".format(lower)))
+    while upper < lower or upper > 127:
+        print("Please enter a valid number!")
+        upper = int(input("Enter an upper number between {} and 127:".format(lower)))
+    return lower, upper
+
+try:
+    while True:
+        a, b = get_lower_upper()
+        number = int(input("Enter a number between %s and %s or any letter to quit: " % (a, b)))
+        if a <= number <= b:
+            print("The character for {} is {}".format(number, chr(number)))
+        else:
+            print("Error - Number must be between %s and %s " % (LOWER, UPPER))
+        break
+except ValueError:
+    print("Goodbye")
+
+
+# try:
+#     while True:
+#         a, b = get_lower_upper()
+#         number = int(input("Enter a number between %s and %s or any letter to quit: " % (a, b)))
+#         if LOWER <= number <= UPPER:
+#             print("The character for {} is {}".format(number, chr(number)))
+#         else:
+#             print("Error - Number must be between %s and %s " % (LOWER, UPPER))
+# except ValueError:
+#     print("Goodbye")
+
+# UPPER = 127
+# LOWER = 33
+# number_of_columns = int(input("Enter how many columns you'd like to display the values: "))
+# number_of_rows = (UPPER-LOWER+1)//number_of_columns
+# for i in range(number_of_rows):
+#     for j in range(number_of_columns):
+#         ascii_number = LOWER + (i * number_of_columns) + j
+#         # print("{:3},{:3} {}".format(i, j, chr(j)), end=" ")
+#         print("{:3} {}".format(ascii_number, chr(ascii_number)), end=" ")
+#     print()
+# ascii_number += 1
+# while ascii_number <= UPPER:
+#     print("{:3} {}".format(ascii_number, chr(ascii_number)), end=" ")
+#     ascii_number += 1
