72a0f7b87d5a7711f096e04893e5e4784bb8d9eb
Micheal Brady-Mahoney <micheal.bradymahoney@my.jcu.edu.au>
2017-03-15 15:02:50 +1000

    Copied from previous week as per practical instructions

--- Prac 03/wordGenerator.py
+++ Prac 03/wordGenerator.py
@@ -5,26 +5,26 @@ import random
 VOWELS = "aeiou"
 CONSONANTS = "bcdfghjklmnpqrstvwxyz"
 
-# word_format = str(input("Enter word format. \nC - Consonant \nV - Vowel \n>>>").lower())
-# word = ""
-# for kind in word_format:
-#     if kind == "c":
-#         word += random.choice(CONSONANTS)
-#     else:
-#         word += random.choice(VOWELS)
-#
-# print(word)
-
-OPTIONS = 'c' + 'v'
-word_format = ""
+word_format = str(input("Enter word format. \nC - Consonant \nV - Vowel \n>>>").lower())
 word = ""
-numLetters = int(input("How many letter word would you like?"))
-for i in range(numLetters):
-        word_format += random.choice(OPTIONS)
-print(word_format)
 for kind in word_format:
     if kind == "c":
         word += random.choice(CONSONANTS)
     else:
         word += random.choice(VOWELS)
+
 print(word)
+
+# OPTIONS = 'c' + 'v'
+# word_format = ""
+# word = ""
+# numLetters = int(input("How many letter word would you like?"))
+# for i in range(numLetters):
+#         word_format += random.choice(OPTIONS)
+# print(word_format)
+# for kind in word_format:
+#     if kind == "c":
+#         word += random.choice(CONSONANTS)
+#     else:
+#         word += random.choice(VOWELS)
+# print(word)
