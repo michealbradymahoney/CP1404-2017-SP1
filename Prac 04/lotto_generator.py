b31fc5e55d5f9725bcd9aad585c330d8e5c202d1
Micheal Brady-Mahoney <micheal.bradymahoney@my.jcu.edu.au>
2017-03-31 13:34:22 +1000

    complete loto generator... output not correctly formatted

--- Prac 04/lotto_generator.py
+++ Prac 04/lotto_generator.py
@@ -2,11 +2,14 @@ __author__ = 'Micheal Brady-Mahoney'
 
 import random
 
-line = []
 no_of_games = int(input("How many quick picks? "))
 
 for games in range(0, no_of_games):
-    line = sorted([random.randint(1, 46) for i in range(0, 6)])
-    print(line)
+    line = []
+    for i in range(0, 6):
+        generated_number = random.randint(1, 45)
+        while generated_number in line:
+            generated_number = random.randint(1, 45)
+        line.append(generated_number)
+    print(*(sorted(line)), sep=' ')
 
-# TODO Make sure there are no duplicates. If so, generate new list
